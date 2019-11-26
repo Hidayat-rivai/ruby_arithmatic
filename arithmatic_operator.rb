974e1c5aaa35e15eb65e8697ac36bdccdd18108a
Hidayat Riva'i <stu.dent.palawija@gmail.com>
2019-11-26 21:37:31 +0700

    ruby arithmatic

---
+++ arithmatic_operator.rb
@@ -0,0 +1,15 @@
+A = 10
+B = 4
+
+puts "A = #{A}"
+puts "B = #{B}"
+puts "A + B = #{A + B}"
+puts "A - B = #{A - B}"
+puts "A * B = #{A * B}"
+puts "A % B = #{A % B}"
+puts "A ** B = #{A ** B}"
+puts "A / B = #{A / B}"
+
+C=10.0
+puts "C = #{C}"
+puts "C / B = #{C / B}"
